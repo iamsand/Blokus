2b6d5254372875f996e25af9ccab2ed60f68c26d
iamsand_laptop <cosinecurve@gmail.com>
2014-07-17 19:15:24 -0700

    Minor changes.

--- src/Players/Human.java
+++ src/Players/Human.java
@@ -1,9 +1,13 @@
 package Players;
 
+import java.util.Scanner;
+
 import framework.IPlayer;
 
 public class Human implements IPlayer{
 
+	Scanner sc = new Scanner(System.in);
+	
 	@Override
 	public void getAction() {
 		// TODO Auto-generated method stub
--- src/framework/Board.java
+++ src/framework/Board.java
@@ -1,17 +1,32 @@
 package framework;
 
 public class Board {
-	
+
 	// This will represent the visual board.
-	Colors[][] b;
-	// This will keep track of the pieces.
-	
-	
-	public Board(){
+	Colors[][]	b;
+
+	// This will keep track of the individual pieces.
+	// TODO: put some sort of map here
+
+	public Board() {
 		b = new Colors[20][20];
+		for (int r = 0; r < 20; r++)
+			for (int c = 0; c < 20; c++) 
+				b[r][c] = Colors.NULL;
 	}
-	
-	public boolean addPiece(){
+
+	public boolean addShape(int[][] s, int x, int y) {
+		// TODO
 		return false;
+	}
+
+	public void conPrint() {
+		for (int r = 0; r < 20; r++) {
+			for (int c = 0; c < 20; c++) {
+				System.out.print("[" + b[r][c] + "] ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }

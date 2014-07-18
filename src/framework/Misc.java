48f9f2886b4841fee28a77082626638d7a1834d2
irfing <irvingc@hotmail.com>
2014-07-18 13:22:05 -0700

    added shape data for tetrominoes

--- src/framework/Misc.java
+++ src/framework/Misc.java
@@ -1,12 +1,34 @@
 package framework;
 
 public class Misc {
+    
 	// This is the hardcoding of the pieces. The indexes are [piece number][coordinate index][x,y coordinate]
     // http://en.wikipedia.org/wiki/File:BlokusTiles.svg
-	public final static int[][][]	mono	= new int[][][] { { { 0, 0 } } };
-	public final static int[][][]	domi	= new int[][][] { { { 0, 0 }, { 1, 0 } } };
-	public final static int[][][]	trom	= new int[][][] { { { 0, 0 }, { 1, 0 }, { 1, -1 } }, { { 0, 0 }, { 1, 0 }, { 2, 0 } } };
+    
+	public final static int[][][] mono = new int[][][] {
+        { { 0, 0 } }
+    };
+    
+	public final static int[][][] domi = new int[][][] {
+        { { 0, 0 }, { 1, 0 } }
+    };
+    
+	public final static int[][][] trom = new int[][][] {
+        { { 0, 0 }, { 1, 0 }, { 1, -1 } },
+        { { 0, 0 }, { 1, 0 }, { -1, 0 } }
+    };
+    
+	public final static int[][][] tetr = new int[][][] {
+        { { 0, 0 }, { 1, 0 }, { 1, 1 }, { 0, 1 } },
+        { { 0, 0 }, { 1, 0 }, { -1, 0 }, { 0, 1 } },
+        { { -1, -1 }, { 0, -1 }, { 1, -1 }, { 1, 0 } },
+        { { 0, 0 }, { 1, 0 }, { 2, 0 }, { -1, 0 } },
+        { { 0, 0 }, { -1, 0 }, { 0, 1 }, { 1, 1 } }
+    };
+    
 	// TODO
-	public final static int[][][]	tetr	= new int[][][] { { {} } };
-	public final static int[][][]	pent	= new int[][][] { { {} } };
+	public final static int[][][] pent = new int[][][] {
+        { {} }
+    };
+    
 }
--- src/framework/Tester.java
+++ src/framework/Tester.java
@@ -3,7 +3,7 @@ package framework;
 public class Tester {
     
     public static void main(String[] args) {
-        Shape s = new Shape(Misc.domi[0]);
+        Shape s = new Shape(Misc.tetr[2]);
         System.out.println(s.toConsoleString());
         s.rotateCW(1);
         System.out.println(s.toConsoleString());

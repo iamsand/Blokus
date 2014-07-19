c01579862906f6b0fc82002ccb9210e0ef36c72d
irfing <irvingc@hotmail.com>
2014-07-19 15:20:02 -0700

    add HumanGraphicalPlayer (wip)

---
+++ src/Players/HumanGraphicalPlayer.java
@@ -0,0 +1,38 @@
+package Players;
+
+import framework.*;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HumanGraphicalPlayer implements IPlayer {
+
+    public void startGame(int boardWidth, int boardHeight, int numPlayers, Color color) {
+        
+    }
+
+    public Action getAction(Board.PlayerView board, List<Shape> hand) {
+        BlokusGamePane pane = this.new BlokusGamePane(board, hand);
+        
+        JFrame frame = new JFrame("Blokus");
+        frame.setContentPane(pane);
+        frame.pack();
+        frame.show();
+        
+        return pane.getAction();
+    }
+    
+    private class BlokusGamePane extends JPanel {
+        
+        public BlokusGamePane(Board.PlayerView board, List<Shape> hand) {
+            
+        }
+        
+        public Action getAction() {
+            // TODO: implement blocking until user has played a move
+            return null;
+        }
+        
+    }
+    
+}

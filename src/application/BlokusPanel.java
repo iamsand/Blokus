889975450d85cf91220a43866920b05b18e99fb9
irfing <irvingc@hotmail.com>
2014-07-21 21:26:07 -0700

    desktop interface now functional, you can play games against yourself or the AI

--- src/application/BlokusPanel.java
+++ src/application/BlokusPanel.java
@@ -2,9 +2,14 @@ package application;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
@@ -13,37 +18,72 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.ListSelectionModel;
 
 import framework.Action;
 import framework.Board;
 import framework.Board.PlayerView;
 import framework.Color;
 import framework.PieceName;
+import framework.Shape;
 
 public class BlokusPanel extends JPanel {
 	
-	private static final int CELL_SIZE = 20;
+	private static final int CELL_SIZE = 24;
 	private static final int  GAP_SIZE =  1;
 	
 	private boolean waitingForAction = false;
+	private Color actionColor = Color.NULL;
 	private Action action;
 	
-	private JLabel[][] grid = null;
+	private BlokusCell[][] grid = null;
 	
 	private final DefaultListModel<PieceName> shapeListModel = new DefaultListModel<PieceName>();
 	private final JList<PieceName> shapeList = new JList<PieceName>(shapeListModel);
 	private final JButton btnRotate = new JButton("CW90");
+	private final JButton btnReflect = new JButton("Refl.");
 	
 	private final JPanel boardPanel = new JPanel();
 	private final JPanel selectionPanel = new JPanel();
 	
+	private final LinkedList<BlokusCell> highlighted = new LinkedList<BlokusCell>();
+	
+	private int pieceRotation = 0;
+	private boolean pieceReflected = false;
+	
 	public BlokusPanel() {
 		this.setLayout(new BorderLayout());
 		
-		this.selectionPanel.setLayout(new BorderLayout());
+		this.shapeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.btnRotate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				BlokusPanel.this.pieceRotation = (BlokusPanel.this.pieceRotation + 1) % 4; 
+			}
+		});
+		this.btnReflect.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				BlokusPanel.this.pieceReflected = !BlokusPanel.this.pieceReflected;
+			}
+		});
 		
-		this.selectionPanel.add(this.shapeList, BorderLayout.CENTER);
-		this.selectionPanel.add(this.btnRotate, BorderLayout.PAGE_END);
+		this.selectionPanel.setLayout(new GridBagLayout());
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 1.0;
+		
+		this.selectionPanel.add(this.shapeList, c);
+
+		c.gridy = 1;
+		c.weighty = 0;
+		this.selectionPanel.add(this.btnRotate, c);
+		
+		c.gridy = 2;
+		this.selectionPanel.add(this.btnReflect, c);
 		
 		this.add(this.boardPanel, BorderLayout.CENTER);
 		this.add(this.selectionPanel, BorderLayout.LINE_END);
@@ -53,15 +93,21 @@ public class BlokusPanel extends JPanel {
 		if (this.grid == null || this.grid.length != view.getHeight() || this.grid[0].length != view.getWidth()) {
 			this.boardPanel.removeAll();
 			this.boardPanel.setLayout(new GridLayout(view.getHeight(), view.getWidth(), BlokusPanel.GAP_SIZE, BlokusPanel.GAP_SIZE));
-			this.grid = new JLabel[view.getHeight()][view.getWidth()];
+			this.grid = new BlokusCell[view.getHeight()][view.getWidth()];
 			for (int i = 0; i < view.getHeight(); i++) {
 				for (int j = 0; j < view.getWidth(); j++) {
-					JLabel label = new JLabel();
-					label.setPreferredSize(new Dimension(BlokusPanel.CELL_SIZE, BlokusPanel.CELL_SIZE));
-					label.setOpaque(true);
-					label.addMouseListener(new MouseListener() {
+					BlokusCell cell = new BlokusCell(i, j);
+					cell.setPreferredSize(new Dimension(BlokusPanel.CELL_SIZE, BlokusPanel.CELL_SIZE));
+					cell.setOpaque(true);
+					cell.addMouseListener(new MouseListener() {
 						@Override
 						public void mouseClicked(MouseEvent e) {
+							if (BlokusPanel.this.waitingForAction) {
+								BlokusPanel.this.waitingForAction = false;
+								synchronized(BlokusPanel.this) {
+									BlokusPanel.this.notify();
+								}
+							}
 						}
 
 						@Override
@@ -70,20 +116,45 @@ public class BlokusPanel extends JPanel {
 
 						@Override
 						public void mouseReleased(MouseEvent e) {
+							this.mouseExited(null);
 						}
 
 						@Override
 						public void mouseEntered(MouseEvent e) {
-							label.setBackground(label.getBackground().brighter().brighter());
+							PieceName selected = BlokusPanel.this.shapeList.getSelectedValue();
+							if (selected != null) {
+								Shape shape = Shape.createShape(selected);
+								shape = shape.rotateCW(BlokusPanel.this.pieceRotation);
+								if (BlokusPanel.this.pieceReflected) {
+									shape = shape.reflectHorizontal();
+								}
+								
+								int[][] coordinates = shape.getCoordinates();
+								for (int[] coordinate : coordinates) {
+									int r = cell.row - coordinate[1];
+									int c = cell.column + coordinate[0];
+									
+									if (r >= 0 && r < view.getHeight() && c >= 0 && c < view.getWidth()) {
+										BlokusCell highlightedCell = BlokusPanel.this.grid[r][c];
+										BlokusPanel.this.highlighted.add(highlightedCell);
+										highlightedCell.setBackground(cell.getBackground().brighter().brighter());
+									}
+								}
+								
+								BlokusPanel.this.action = new Action(shape, BlokusPanel.this.actionColor, cell.column, view.getHeight() - 1 - cell.row);
+							}
 						}
 
 						@Override
 						public void mouseExited(MouseEvent e) {
-							label.setBackground(label.getBackground().darker().darker());
+							while (!BlokusPanel.this.highlighted.isEmpty()) {
+								BlokusCell highlightedCell = BlokusPanel.this.highlighted.pop();
+								highlightedCell.setBackground(BlokusPanel.this.convertColor(view.getColor(highlightedCell.column, view.getHeight() - 1 - highlightedCell.row)).darker().darker());
+							}
 						}
 					});
-					this.grid[i][j] = label;
-					this.boardPanel.add(label);
+					this.grid[i][j] = cell;
+					this.boardPanel.add(cell);
 				}
 			}
 		}
@@ -111,6 +182,8 @@ public class BlokusPanel extends JPanel {
 		for (PieceName piece : hand) {
 			this.shapeListModel.addElement(piece);
 		}
+		this.shapeList.setSelectedIndex(0);
+		this.actionColor = color;
 		
 		synchronized (this) {
 			while (waitingForAction) {
@@ -122,6 +195,20 @@ public class BlokusPanel extends JPanel {
 		}
 		
 		return this.action;
+	}
+	
+	private static class BlokusCell extends JLabel {
+		
+		public final int row;
+		public final int column;
+		
+		public BlokusCell(int row, int column) {
+			super();
+			
+			this.row = row;
+			this.column = column;
+		}
+		
 	}
 	
 	public class HumanPlayer implements IPlayer {
